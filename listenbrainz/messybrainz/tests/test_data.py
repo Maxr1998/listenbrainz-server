a2d4a718579f1016bb22737565b6faa2e5916b8f
Kartik Ohri <kartikohri13@gmail.com>
2022-08-11 00:27:15 +0530

    insert old data in new table

--- listenbrainz/messybrainz/tests/test_data.py
+++ listenbrainz/messybrainz/tests/test_data.py
@@ -16,11 +16,9 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA)
 
-import json
-
-from listenbrainz.messybrainz import data
 from listenbrainz import messybrainz
-from listenbrainz.messybrainz.testing import MessyBrainzTestCase
+from listenbrainz.db import timescale
+from listenbrainz.db.testing import TimescaleTestCase
 
 
 recording = {
@@ -44,61 +42,24 @@ recording_diff_case = {
 }
 
 
-class DataTestCase(MessyBrainzTestCase):
-
-    def test_get_id_from_meta_hash(self):
-        with messybrainz.engine.connect() as connection:
-            recording_msid = data.submit_recording(connection, recording)
-            self.assertEqual(recording_msid, str(data.get_id_from_meta_hash(connection, recording)))
+class DataTestCase(TimescaleTestCase):
 
     def test_submit_recording(self):
-        with messybrainz.engine.connect() as connection:
-            recording_msid = data.submit_recording(connection, recording)
-            self.assertEqual(recording_msid, str(data.get_id_from_recording(connection, recording)))
-
-    def test_get_artist_credit(self):
-        with messybrainz.engine.connect() as connection:
-            recording_msid = data.submit_recording(connection, recording)
-            artist_msid = data.get_artist_credit(connection, recording['artist'])
-            recording_data = data.load_recordings_from_msids(connection, [recording_msid])[0]
-            self.assertEqual(artist_msid, recording_data['ids']['artist_msid'])
-
-    def test_get_release(self):
-        with messybrainz.engine.connect() as connection:
-            recording_msid = data.submit_recording(connection, recording)
-            release_msid = data.get_release(connection, recording['release'])
-            recording_data = data.load_recordings_from_msids(connection, [recording_msid])[0]
-            self.assertEqual(release_msid, recording_data['ids']['release_msid'])
-
-    def test_add_artist_credit(self):
-        with messybrainz.engine.connect() as connection:
-            artist_msid = data.add_artist_credit(connection, 'Kanye West')
-            self.assertEqual(artist_msid, data.get_artist_credit(connection, 'Kanye West'))
-
-    def test_add_release(self):
-        with messybrainz.engine.connect() as connection:
-            release_msid = data.add_release(connection, 'The College Dropout')
-            self.assertEqual(release_msid, data.get_release(connection, 'The College Dropout'))
+        with timescale.engine.connect() as connection:
+            recording_msid = messybrainz.submit_recording(connection, recording)
+            self.assertEqual(recording_msid, str(messybrainz.get_id_from_recording(connection, recording)))
 
     def test_add_recording_different_cases(self):
         """ Tests that recordings with only case differences get the same MessyBrainz ID.
         """
-        with messybrainz.engine.connect() as connection:
-            msid1 = data.submit_recording(connection, recording)
-            msid2 = str(data.get_id_from_recording(connection, recording_diff_case))
+        with timescale.engine.connect() as connection:
+            msid1 = messybrainz.submit_recording(connection, recording)
+            msid2 = str(messybrainz.get_id_from_recording(connection, recording_diff_case))
             self.assertEqual(msid1, msid2)
 
     def test_load_recordings_from_msids(self):
-        with messybrainz.engine.connect() as connection:
-            recording_msid = data.submit_recording(connection, recording)
-            result = data.load_recordings_from_msids(connection, [recording_msid])[0]
+        with timescale.engine.connect() as connection:
+            recording_msid = messybrainz.submit_recording(connection, recording)
+            result = messybrainz.load_recordings_from_msids(connection, [recording_msid])[0]
             self.assertDictEqual(result['payload'], recording)
 
-    def test_convert_to_messybrainz_json(self):
-        sorted_keys, transformed_json = data.convert_to_messybrainz_json(recording)
-        result = json.loads(transformed_json)
-        self.assertEqual(result['artist'], recording['artist'].lower())
-        self.assertEqual(result['release'], recording['release'].lower())
-        self.assertEqual(result['title'], recording['title'].lower())
-        self.assertEqual(result['additional_info']['key1'], recording['additional_info']['key1'].lower())
-        self.assertDictEqual(json.loads(sorted_keys), recording)
