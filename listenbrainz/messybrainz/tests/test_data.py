bd0132582235a24069bea8792ca218c2c9bc957b
Kartik Ohri <kartikohri13@gmail.com>
2022-08-17 15:59:22 +0530

    Migrate MsB to TS

--- listenbrainz/messybrainz/tests/test_data.py
+++ listenbrainz/messybrainz/tests/test_data.py
@@ -31,7 +31,7 @@ recording = {
     'recording_mbid': "5465ca86-3881-4349-81b2-6efbd3a59451"
 }
 
-recording_diff_case = {
+recording2 = {
     'artist': 'FRANK OCEAN',
     'release': 'BLoNd',
     'title': 'PReTtY SWEET',
@@ -46,20 +46,28 @@ class DataTestCase(TimescaleTestCase):
 
     def test_submit_recording(self):
         with timescale.engine.connect() as connection:
-            recording_msid = messybrainz.submit_recording(connection, recording)
-            self.assertEqual(recording_msid, str(messybrainz.get_id_from_recording(connection, recording)))
+            title, artist, release = recording["title"], recording["artist"], recording["release"]
+            recording_msid = messybrainz.submit_recording(connection, title, artist, release)
+            self.assertEqual(recording_msid, str(messybrainz.get_msid(connection, title, artist, release)))
 
     def test_add_recording_different_cases(self):
         """ Tests that recordings with only case differences get the same MessyBrainz ID.
         """
         with timescale.engine.connect() as connection:
-            msid1 = messybrainz.submit_recording(connection, recording)
-            msid2 = str(messybrainz.get_id_from_recording(connection, recording_diff_case))
+            title1, artist1, release1 = recording["title"], recording["artist"], recording["release"]
+            title2, artist2, release2 = recording2["title"], recording2["artist"], recording2["release"]
+            msid1 = messybrainz.submit_recording(connection, title1, artist1, release1)
+            msid2 = str(messybrainz.get_msid(connection, title2, artist2, release2))
             self.assertEqual(msid1, msid2)
 
     def test_load_recordings_from_msids(self):
         with timescale.engine.connect() as connection:
-            recording_msid = messybrainz.submit_recording(connection, recording)
+            title, artist, release = recording["title"], recording["artist"], recording["release"]
+            recording_msid = messybrainz.submit_recording(connection, title, artist, release)
             result = messybrainz.load_recordings_from_msids(connection, [recording_msid])[0]
-            self.assertDictEqual(result['payload'], recording)
-
+            self.assertDictEqual(result, {
+                "msid": recording_msid,
+                "title": title,
+                "artist": artist,
+                "release": release
+            })
