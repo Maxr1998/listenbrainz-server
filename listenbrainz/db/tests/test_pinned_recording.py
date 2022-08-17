159d448d52ba8b777618f09f8706ef21c7503a6a
Kartik Ohri <kartikohri13@gmail.com>
2022-08-17 16:25:19 +0530

    fix msid access

--- listenbrainz/db/tests/test_pinned_recording.py
+++ listenbrainz/db/tests/test_pinned_recording.py
@@ -107,8 +107,7 @@ class PinnedRecDatabaseTestCase(DatabaseTestCase, TimescaleTestCase):
             }
         ]
 
-        submitted_data = msb_db.insert_all_in_transaction(recordings)
-        msids = [x["msid"] for x in submitted_data]
+        msids = msb_db.insert_all_in_transaction(recordings)
 
         with ts.engine.connect() as connection:
             query = """
