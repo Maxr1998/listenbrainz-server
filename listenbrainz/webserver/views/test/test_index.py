ab8b78df4ca2360ec0213b39d93b234c0d5ff2fe
Kartik Ohri <kartikohri13@gmail.com>
2022-08-24 22:43:14 +0530

    Fix tests

--- listenbrainz/webserver/views/test/test_index.py
+++ listenbrainz/webserver/views/test/test_index.py
@@ -1,7 +1,6 @@
 from unittest import mock
 from unittest.mock import MagicMock
 
-import ujson
 from flask import url_for
 from flask_login import login_required
 from requests.exceptions import HTTPError
@@ -10,11 +9,12 @@ from werkzeug.exceptions import BadRequest, InternalServerError, NotFound
 import listenbrainz.db.user as db_user
 import listenbrainz.webserver.login
 from listenbrainz.db.testing import DatabaseTestCase
+from listenbrainz.tests.integration import IntegrationTestCase
 from listenbrainz.webserver import create_web_app
 from listenbrainz.webserver.testing import ServerTestCase
 
 
-class IndexViewsTestCase(ServerTestCase, DatabaseTestCase):
+class IndexViewsTestCase(IntegrationTestCase):
 
     def setUp(self):
         ServerTestCase.setUp(self)
